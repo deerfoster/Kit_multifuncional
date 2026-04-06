6f5f25f7c5604b25e53bafacf97f5444135be95d
Xari <31975280+deerfoster@users.noreply.github.com>
2026-04-06 03:22:44 -0400

    . funcion para leer archivos

--- analyzer.py
+++ analyzer.py
@@ -1,7 +1,20 @@
+#Funcion para leer los archivos
+def leer_archivo(nombre_archivo):
+    lineas = []
+    try:
+        with open(nombre_archivo, 'r', encoding='utf-8') as f:
+            for linea in f:
+                lineas.append(linea.strip())
+            return lineas
+    except:
+        print("El archivo no existe o no coincide. ")
+        return []
+
 #Import del regex (regular expressions) en python
 import re
 
-#Esta es la variable que contiene todos los patrones que queremos identificar
-#Uppercase (A-Z) , Lowercase (a-z), numbers (0-9) , carácteres especiales , direcciones comunes
+#Variable con los patrones a identificar
 patrones = "[a-zA-Z0-9]+@[a-zA-Z]+\.(com|edu|net)"
 
+#Input para el buscador
+user = input("...")
