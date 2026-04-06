2d943f6011abab895c28a7c3aeedf9c341c380f0
Xari <31975280+deerfoster@users.noreply.github.com>
2026-04-06 02:55:48 -0400

    import regex y variable con los patrones

---
+++ analyzer.py
@@ -0,0 +1,7 @@
+#Import del regex (regular expressions) en python
+import re
+
+#Esta es la variable que contiene todos los patrones que queremos identificar
+#Uppercase (A-Z) , Lowercase (a-z), numbers (0-9) , carácteres especiales , direcciones comunes
+patrones = "[a-zA-Z0-9]+@[a-zA-Z]+\.(com|edu|net)"
+
